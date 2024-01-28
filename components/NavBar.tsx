0dfd393ef378a15e19ba8d29cf1d0e5b593f2d00
Jordan <jordantseng1024@gmail.com>
2024-01-28 12:38:04 +0900

    feat(home): add navbar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import Link from 'next/link'
+import { HomeIcon, ArrowDownWideNarrowIcon, ShoppingCartIcon, UserRoundIcon } from 'lucide-react'
+
+import IconCard from '@/components/IconCard'
+import { usePathname } from 'next/navigation'
+
+const NavBar = () => {
+  const pathName = usePathname()
+
+  const navItems = [
+    {
+      title: '首頁',
+      href: '/',
+      Icon: <HomeIcon className="h-10 w-10 p-2" />,
+    },
+    {
+      title: '分類',
+      href: '/products',
+      Icon: <ArrowDownWideNarrowIcon className="h-10 w-10 p-2" />,
+    },
+    {
+      title: '購物車',
+      href: '/shopping-cart',
+      Icon: <ShoppingCartIcon className="h-10 w-10 p-2" />,
+    },
+    {
+      title: '我的',
+      href: '/member',
+      Icon: <UserRoundIcon className="h-10 w-10 p-2" />,
+    },
+  ]
+
+  return (
+    <nav className="h-22 fixed bottom-0 flex w-full max-w-[640px] justify-around bg-white">
+      {navItems.map((item, index) => {
+        return (
+          <Link key={index} href={item.href}>
+            <IconCard
+              title={item.title}
+              Icon={item.Icon}
+              isActive={item.href === '/' ? pathName === '/' : pathName.includes(item.href)}
+            />
+          </Link>
+        )
+      })}
+    </nav>
+  )
+}
+
+export default NavBar
