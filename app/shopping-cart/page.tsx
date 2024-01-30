21c830334404776efefaefd438a9d8472e42782a
Gary.lin <gary.lin@clinicodeMacBook-Pro.local>
2024-01-30 16:48:28 +0800

    style(shopping-cart): adjust style again

--- app/shopping-cart/page.tsx
+++ app/shopping-cart/page.tsx
@@ -6,6 +6,7 @@ import NavBar from '@/components/NavBar'
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
 import { useCartContext } from '@/context/CartContext'
+import Link from 'next/link'
 import React, { useState } from 'react'
 import { useImmer } from 'use-immer'
 
@@ -83,7 +84,7 @@ const ShoppingCartPage = () => {
         <div className="mb-5 flex w-full items-center justify-center gap-x-1.5 p-2">
           <MerchandiseCard
             id={12345}
-            className="h-64 w-[50%] md:w-auto"
+            className="h-72 w-[50%] md:w-auto"
             imgUrl="https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$"
             title="PS5"
             tags={['game', 'tv']}
@@ -93,7 +94,7 @@ const ShoppingCartPage = () => {
           />
           <MerchandiseCard
             id={55555}
-            className="h-64 w-[50%] md:w-auto"
+            className="h-72 w-[50%] md:w-auto"
             imgUrl="https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$"
             title="PS5 GGGHHGHGHGHGHGHGHGHGHGHG"
             tags={['game', 'tv']}
@@ -117,7 +118,9 @@ const ShoppingCartPage = () => {
               <span>總計：</span>
               <span className="text-lg font-semibold text-red-400">${total}</span>
             </div>
-            <Button className="w-[4/12] rounded-3xl bg-primary">結帳</Button>
+            <Link href="/confirm-bill">
+              <Button className="w-[4/12] rounded-3xl bg-primary">結帳</Button>
+            </Link>
           </div>
         </div>
       </div>
