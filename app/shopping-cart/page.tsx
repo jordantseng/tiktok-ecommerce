4487fe7853d06e7a1b561516aa11eb4c0fbc11d8
Gary.lin <gary.lin@clinicodeMacBook-Pro.local>
2024-01-30 16:15:34 +0800

    style(shopping-cart): adjust style

--- app/shopping-cart/page.tsx
+++ app/shopping-cart/page.tsx
@@ -83,7 +83,7 @@ const ShoppingCartPage = () => {
         <div className="mb-5 flex w-full items-center justify-center gap-x-1.5 p-2">
           <MerchandiseCard
             id={12345}
-            className="max-h-[320px] w-[50%] md:w-auto"
+            className="h-64 w-[50%] md:w-auto"
             imgUrl="https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$"
             title="PS5"
             tags={['game', 'tv']}
@@ -93,7 +93,7 @@ const ShoppingCartPage = () => {
           />
           <MerchandiseCard
             id={55555}
-            className="max-h-[320px] w-[50%] md:w-auto"
+            className="h-64 w-[50%] md:w-auto"
             imgUrl="https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$"
             title="PS5 GGGHHGHGHGHGHGHGHGHGHGHG"
             tags={['game', 'tv']}
