21024c8801fc5e0477dc771cbe6dbfe65528bf83
Gary.lin <gary.lin@clinicodeMacBook-Pro.local>
2024-02-01 14:17:31 +0800

    feat(api): add cart api

---
+++ services/cart.ts
@@ -0,0 +1,73 @@
+import Config from '@/lib/configs'
+import { ApiRes } from '@/types/common'
+
+type CartRes = ApiRes<CartData>
+
+type CartsRes = ApiRes<{
+  current_page: number
+  data: CartData[]
+  total: number
+}>
+
+type CartData = {
+  id: number
+  title: string
+  imgs: string
+  company: string
+  expertise: string
+  info: string
+  productitem_title: string
+  productitem_id: number
+}
+
+export const getMyCart = async (): Promise<CartRes> => {
+  const res = await fetch(`${Config.api}/api/membercenter/mycart`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      page: 1,
+      pagesize: 10000,
+    }),
+    next: { revalidate: 60 * 5 },
+  })
+
+  const data = await res.json()
+
+  return data
+}
+
+export const addToCart = async (id: number): Promise<CartsRes> => {
+  const res = await fetch(`${Config.api}/api/membercenter/mycart/store`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      productitem_id: id,
+    }),
+    next: { revalidate: 60 * 5 },
+  })
+
+  const data = await res.json()
+
+  return data
+}
+
+export const deleteFromCart = async (id: number): Promise<CartsRes> => {
+  const res = await fetch(`${Config.api}/api/membercenter/mycart/destory`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      del: id,
+    }),
+    next: { revalidate: 60 * 5 },
+  })
+
+  const data = await res.json()
+
+  return data
+}
