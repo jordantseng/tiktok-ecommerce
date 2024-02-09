d0e61a55dc7f1fedfffd6c39e72d8af74fbdb3e8
Jordan <jordantseng1024@gmail.com>
2024-02-09 23:49:06 +0900

    feat(search): add localstorage for search history

--- app/search/SearchHistory.tsx
+++ app/search/SearchHistory.tsx
@@ -1,15 +1,53 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 import { Trash2Icon } from 'lucide-react'
 
+import { Badge } from '@/components/ui/badge'
+
 const SearchHistory = () => {
+  const [historyTerms, setHistoryTerms] = useState<string[]>([])
+  const router = useRouter()
+
+  const stringifiedHistoryTerms = localStorage.getItem('historyTerms') ?? '[]'
+
+  useEffect(() => {
+    const historyTerms = JSON.parse(stringifiedHistoryTerms) as string[]
+
+    setHistoryTerms(historyTerms)
+  }, [stringifiedHistoryTerms])
+
+  const handleClick = (term: string) => {
+    router.push(`/search?q=${term}`)
+  }
+
+  const handleClearHistoryTerms = () => {
+    localStorage.removeItem('historyTerms')
+    setHistoryTerms([])
+  }
+
   return (
-    <div className="flex justify-between">
-      <h4 className="mb-2 scroll-m-20 text-xl font-normal tracking-tight">歷史搜索</h4>
-      <button>
-        <Trash2Icon className="text-gray-400" />
-      </button>
-    </div>
+    <>
+      <div className="flex justify-between">
+        <h4 className="mb-2 scroll-m-20 text-xl font-normal tracking-tight">歷史搜索</h4>
+        <button onClick={handleClearHistoryTerms}>
+          <Trash2Icon className="text-gray-400" />
+        </button>
+      </div>
+      <div className="flex flex-wrap gap-2">
+        {historyTerms.map((term, index) => (
+          <Badge
+            className="rounded-xl"
+            variant="secondary"
+            key={index}
+            onClick={() => handleClick(term)}
+          >
+            {term}
+          </Badge>
+        ))}
+      </div>
+    </>
   )
 }
 
