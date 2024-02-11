100db3ee59e3574f8cba40235fb39bffe2ed2f68
john👽cheng <john_cheng@htc.com>
2024-02-11 08:43:19 +0800

    feat(iconcard.tsx): add onClick props to enhance usage

--- components/IconCard.tsx
+++ components/IconCard.tsx
@@ -4,11 +4,12 @@ type IconCardProps = {
   title: string
   Icon: React.ReactNode
   isActive?: boolean
+  onClick?: () => void
 }
 
-const IconCard = ({ title, Icon, isActive = false }: IconCardProps) => {
+const IconCard = ({ title, Icon, isActive = false, onClick }: IconCardProps) => {
   return (
-    <div className="flex flex-col items-center justify-between">
+    <div className="flex flex-col items-center justify-between" onClick={onClick}>
       <div className={`${isActive ? 'text-primary' : ''}`}>{Icon}</div>
       <h4
         className={`${cn('md:text-md mb-2 scroll-m-20 text-sm font-normal md:tracking-wider', {
