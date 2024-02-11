0d78aa05bc1ba35ee15abad4e554fc142f9c166f
john👽cheng <john_cheng@htc.com>
2024-02-11 09:11:55 +0800

    feat: add hover and click effect

--- components/IconCard.tsx
+++ components/IconCard.tsx
@@ -9,12 +9,18 @@ type IconCardProps = {
 
 const IconCard = ({ title, Icon, isActive = false, onClick }: IconCardProps) => {
   return (
-    <div className="flex flex-col items-center justify-between" onClick={onClick}>
+    <div
+      className={cn('flex flex-col items-center justify-between transition-all', {
+        'cursor-pointer': onClick,
+        'hover:text-primary': onClick,
+      })}
+      onClick={onClick}
+    >
       <div className={`${isActive ? 'text-primary' : ''}`}>{Icon}</div>
       <h4
-        className={`${cn('md:text-md mb-2 scroll-m-20 text-sm font-normal md:tracking-wider', {
+        className={cn('md:text-md mb-2 scroll-m-20 text-sm font-normal md:tracking-wider', {
           'text-primary': isActive,
-        })}`}
+        })}
       >
         {title}
       </h4>
