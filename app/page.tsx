b717e670d37be350975c08c420f549dd4579d43c
john👽cheng <john_cheng@htc.com>
2024-02-12 15:23:56 +0800

    feat: update login and register layout

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function HomePage() {
       <header className="sticky top-0 z-10 flex items-center justify-between bg-white px-4 py-4">
         <h4 className="scroll-m-20 text-xl font-normal tracking-tight">天服能量購物商城</h4>
         <div className="flex items-center gap-4">
-          <MessageSquareMoreIcon />
-          <MenuIcon onClick={() => router.push('/info')} />
+          <MessageSquareMoreIcon className="cursor-pointer" />
+          <MenuIcon className="cursor-pointer" onClick={() => router.push('/info')} />
         </div>
       </header>
       <div className="p-4">
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,21 +5,10 @@ import { ChevronRight } from 'lucide-react'
 import PrevButton from '@/components/PrevButton'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { useRouter } from 'next/navigation'
 
-function AvatarDemo() {
-  return (
-    <Avatar className="h-12 w-12 cursor-pointer transition-all hover:border md:h-20 md:w-20">
-      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
-  )
-}
-
-const LoginPage = () => {
+const Register = () => {
   const router = useRouter()
-
   return (
     <main className="flex min-h-screen flex-col bg-white">
       <header className="sticky top-0 z-10 flex min-h-15 items-center justify-between bg-white p-4">
@@ -28,7 +17,7 @@ const LoginPage = () => {
 
       <div className="flex flex-1 flex-col justify-between gap-4 p-6 text-sm">
         <div className="flex flex-col gap-4">
-          <span className="text-xl font-bold">手機號碼登入</span>
+          <span className="text-xl font-bold">註冊</span>
 
           <form
             onSubmit={(e) => {
@@ -37,6 +26,10 @@ const LoginPage = () => {
             className="flex flex-col gap-6"
           >
             <div>
+              <Input
+                className="rounded-none border-b border-l-0 border-r-0 border-t-0 bg-transparent px-0 py-6 outline-none"
+                placeholder="請輸入用戶名稱"
+              />
               <Input
                 className="rounded-none border-b border-l-0 border-r-0 border-t-0 bg-transparent px-0 py-6 outline-none"
                 placeholder="請輸入手機號碼"
@@ -52,28 +45,23 @@ const LoginPage = () => {
               </div>
             </div>
             <Button type="submit" variant="default" className="rounded-full">
-              登入
+              註冊
             </Button>
           </form>
 
           <div className="flex w-full">
             <div
               className="m-auto flex cursor-pointer items-center text-gray-500 transition-all hover:text-gray-400"
-              onClick={() => router.push('/register')}
+              onClick={() => router.push('/login')}
             >
-              新用戶註冊
+              已註冊，去登入
               <ChevronRight />
             </div>
           </div>
-        </div>
-
-        <div className="flex w-full flex-col items-center justify-center gap-2 pb-20 text-gray-500">
-          <span>其他登入方式</span>
-          <AvatarDemo />
         </div>
       </div>
     </main>
   )
 }
 
-export default LoginPage
+export default Register
