da23e16d999608fd9287c9529ff2e6f0e78ec2f6
Jordan <jordantseng1024@gmail.com>
2024-02-12 22:45:52 +0900

    refactor(iconcard): add imgurl prop

--- components/IconCard.tsx
+++ components/IconCard.tsx
@@ -1,22 +1,25 @@
+import Image from 'next/image'
+
 import { cn } from '@/lib/utils'
 
 type IconCardProps = {
   title: string
-  Icon: React.ReactNode
   isActive?: boolean
+  Icon?: React.ReactNode
+  imgUrl?: string
   onClick?: () => void
 }
 
-const IconCard = ({ title, Icon, isActive = false, onClick }: IconCardProps) => {
+const IconCard = ({ title, isActive, Icon, imgUrl, onClick }: IconCardProps) => {
   return (
     <div
       className={cn('flex flex-col items-center justify-between transition-all', {
-        'cursor-pointer': onClick,
-        'hover:text-primary': onClick,
+        'cursor-pointer hover:text-primary': !!onClick,
       })}
       onClick={onClick}
     >
-      <div className={`${isActive ? 'text-primary' : ''}`}>{Icon}</div>
+      {Icon && <div className={`${isActive ? 'text-primary' : ''}`}>{Icon}</div>}
+      {imgUrl && <Image alt="title" src={imgUrl} width={24} height={24} className="h-14 w-14" />}
       <h4
         className={cn('md:text-md mb-2 scroll-m-20 text-sm font-normal md:tracking-wider', {
           'text-primary': isActive,
