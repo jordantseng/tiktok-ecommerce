806af9db7dc8b34a3528eea596f02ea22f40e3dc
Gary.lin <gary.lin@clinicodeMacBook-Pro.local>
2024-02-15 10:57:24 +0800

    fix(context): adjust context name to unify

--- app/confirm-order/PaymentSetting.tsx
+++ app/confirm-order/PaymentSetting.tsx
@@ -9,7 +9,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
-import { useWebSettingsContext } from '@/context/InitialContext'
+import { useWebSettingsContext } from '@/context/WebSettingsContext'
 import { CircleDollarSignIcon, CreditCardIcon, StoreIcon, TruckIcon } from 'lucide-react'
 import React from 'react'
 
