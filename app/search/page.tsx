24a8d1353023fd894e3cb1751e86e62d57343064
Jordan <jordantseng1024@gmail.com>
2024-02-20 21:47:33 +0900

    fix: add guard for get products api

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -7,12 +7,12 @@ import Searchbar from '@/components/Searchbar'
 import MerchandiseCard from '@/components/MerchandiseCard'
 import PrevButton from '@/components/PrevButton'
 import Pagination from '@/components/Pagination'
-import { cn } from '@/lib/utils'
 import { getProducts } from '@/services/product'
 import { getCategories } from '@/services/category'
+import { cn } from '@/lib/utils'
 import { paginationGuard } from '@/lib/guard'
 
-const PAGE_SIZE = 1
+const PAGE_SIZE = 4
 
 type SearchPageProps = {
   searchParams: {
@@ -37,8 +37,8 @@ const SearchPage = async ({ searchParams }: SearchPageProps) => {
     kindheadId: type,
     sortName: sortBy,
     sortType: order === 'ascending' ? 'asc' : 'desc',
-    price1: Number(minPrice),
-    price2: Number(maxPrice),
+    price1: Number(minPrice) || undefined,
+    price2: Number(maxPrice) || undefined,
   })
 
   const { data: categories } = await getCategories()
@@ -69,7 +69,7 @@ const SearchPage = async ({ searchParams }: SearchPageProps) => {
                       className="w-full"
                       imgUrl={product.imgs[0]}
                       title={product.title}
-                      tags={product.tags.split(',')}
+                      tags={product.tags?.split(',')}
                       price={product.price}
                       originPrice={product.marketprice}
                       sales={String(product.buycount)}
