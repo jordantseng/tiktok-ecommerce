34ee69c1275798143a00d889c87946a5741ba523
Gary.lin <gary.lin@clinicodeMacBook-Pro.local>
2024-02-21 11:45:16 +0800

    fix(success): adjust success page and cart behavior

--- app/confirm-order/success/page.tsx
+++ app/confirm-order/success/page.tsx
@@ -1,10 +1,27 @@
 'use client'
 
 import Title from '@/components/Title'
+import { Delivery } from '@/context/AddressContext'
+import { useWebSettingsContext } from '@/context/WebSettingsContext'
+import { deliveryMap, handleLabel } from '@/lib/payment'
+import { OrderData, getOrder } from '@/services/order'
 import { CheckIcon } from 'lucide-react'
-import React from 'react'
+import { useSearchParams } from 'next/navigation'
+import React, { useEffect } from 'react'
+import { useImmer } from 'use-immer'
 
 const SuccessPage = () => {
+  const { webSettingsData } = useWebSettingsContext()
+  const searchParams = useSearchParams()
+  const id = searchParams.get('id')
+  const [data, setData] = useImmer<OrderData | null>(null)
+
+  useEffect(() => {
+    if (id) {
+      getOrder(Number(id)).then(({ data }) => setData(data))
+    }
+  }, [id])
+
   return (
     <main className="min-h-screen">
       <Title title="付款" goBackUrl="/" />
@@ -17,26 +34,30 @@ const SuccessPage = () => {
           <div className="rounded-lg bg-white p-4">
             <div className="flex items-center border-b-2 p-4">
               <div className="text-md w-[25%]">訂單編號</div>
-              <div className="text-md flex w-[75%] justify-end">234832534534</div>
+              <div className="text-md flex w-[75%] justify-end">{data?.ordergroupnumber}</div>
             </div>
             <div className="flex items-center border-b-2 p-4">
               <div className="text-md w-[25%]">運送方式</div>
-              <div className="text-md flex w-[75%] justify-end">超商取貨</div>
+              <div className="text-md flex w-[75%] justify-end">
+                {deliveryMap[(data?.LogisticsSubType || 'home-delivery') as Delivery]}
+              </div>
             </div>
             <div className="flex items-center border-b-2 p-4">
               <div className="text-md w-[25%]">付款方式</div>
-              <div className="text-md flex w-[75%] justify-end">信用卡一次付清</div>
+              <div className="text-md flex w-[75%] justify-end">
+                {handleLabel(data?.paystatus || '', webSettingsData || null)}
+              </div>
             </div>
             <div className="flex items-center border-b-2 p-4">
               <div className="text-md w-[25%]">地址</div>
               <div className="text-md flex w-[75%] flex-col justify-end">
-                <div className="flex justify-end">台北市中山區中山北路七段222號</div>
-                <div className="flex justify-end">中山北門市</div>
+                <div className="flex justify-end">{data?.CVSAddress || data?.raddress}</div>
+                <div className="flex justify-end">{data?.CVSStoreName}</div>
               </div>
             </div>
             <div className="flex items-center p-4">
               <div className="text-md w-[25%]">金額</div>
-              <div className="text-md flex w-[75%] justify-end">$18888</div>
+              <div className="text-md flex w-[75%] justify-end">${data?.totalprice}</div>
             </div>
           </div>
         </div>
