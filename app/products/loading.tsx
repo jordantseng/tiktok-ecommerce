fb189f6164301ffbfe0c51197892ab96600be2b3
Jordan <jordantseng1024@gmail.com>
2024-02-23 22:47:50 +0900

    feat(products): add products loading page

---
+++ app/products/loading.tsx
@@ -0,0 +1,35 @@
+import Sidebar from '@/app/products/Sidebar'
+import ProductList from '@/app/products/ProductList'
+import NavBar from '@/components/NavBar'
+import PrevButton from '@/components/PrevButton'
+import Searchbar from '@/components/Searchbar'
+import { Skeleton } from '@/components/ui/skeleton'
+
+const ProductsLoadingPage = () => {
+  return (
+    <main className="mb-16 min-h-screen bg-default">
+      <header className="sticky top-0 z-50 flex items-center gap-3 bg-default p-4">
+        <PrevButton />
+        <Searchbar />
+      </header>
+      <div className="flex min-h-screen w-full">
+        <Sidebar activeType="" items={[]} />
+        <ProductList subSidebarItems={[]}>
+          {Array(5)
+            .fill(null)
+            .map((product) => (
+              <>
+                <div className="flex flex-col space-y-3 p-2">
+                  <Skeleton className="h-[300px] w-full rounded-xl" />
+                </div>
+                <hr className="mx-auto flex w-11/12" />
+              </>
+            ))}
+        </ProductList>
+      </div>
+      <NavBar />
+    </main>
+  )
+}
+
+export default ProductsLoadingPage
