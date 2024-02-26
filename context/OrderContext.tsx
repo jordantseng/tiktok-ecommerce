e7bed40dc395db91dcbd4b3dfc8971a239396779
OInVein <progthrash3305@gmail.com>
2024-02-26 23:09:40 +0800

    feat: add contact to me

--- context/OrderContext.tsx
+++ context/OrderContext.tsx
@@ -1,15 +1,35 @@
 'use client'
 
-import { PropsWithChildren, createContext, useContext, useEffect, useState } from 'react'
+import {
+  ChangeEvent,
+  PropsWithChildren,
+  RefObject,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 import { useImmer } from 'use-immer'
 import { useRouter } from 'next/navigation'
+import { format } from 'date-fns'
 
 import { OrderData, getOrders } from '@/services/order'
+import { createMemberFeedback } from '@/services/feedback'
 import { useAuthContext } from '@/context/AuthContext'
+import { toast } from '@/components/ui/use-toast'
 
 type OrderContextType = {
   orders: OrderData[]
+  contactOrder: OrderData | null
+  contactMessage: string
+  contactTextareaRef: RefObject<HTMLTextAreaElement>
   isLoadingOrders: boolean
+  isContactDialogOpen: boolean
+  handleContactDialogOpen: (order: OrderData) => () => void
+  handleContactDialogClose: () => void
+  handleContactMessageChange: (e: ChangeEvent<HTMLTextAreaElement>) => void
+  handleContactSubmit: () => void
 }
 
 const OrderContext = createContext<OrderContextType | null>(null)
@@ -17,9 +37,13 @@ const OrderContext = createContext<OrderContextType | null>(null)
 export const OrderProvider = ({ children }: PropsWithChildren) => {
   const router = useRouter()
   const { token, user, isPreparingData, refreshUser } = useAuthContext()
+  const contactTextareaRef = useRef<HTMLTextAreaElement>(null)
 
   const [orders, setOrders] = useImmer<OrderData[]>([])
   const [isLoadingOrders, setIsLoadingOrders] = useState(true)
+  const [isContactDialogOpen, setIsContactDialogOpen] = useState(false)
+  const [contactMessage, setContactMessage] = useState('')
+  const [contactOrder, setContactOrder] = useState<OrderData | null>(null)
 
   useEffect(() => {
     if (!token) {
@@ -49,8 +73,83 @@ export const OrderProvider = ({ children }: PropsWithChildren) => {
       })
   }, [setOrders, user, isPreparingData])
 
+  const handleContactDialogOpen = (order: OrderData) => () => {
+    setIsContactDialogOpen(true)
+    setContactOrder(order)
+    setTimeout(() => {
+      contactTextareaRef.current?.focus()
+    })
+  }
+
+  const handleContactDialogClose = () => {
+    setIsContactDialogOpen(false)
+    setContactMessage('')
+    setContactOrder(null)
+  }
+
+  const handleContactMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContactMessage(e.target.value)
+  }
+
+  const handleContactSubmit = async () => {
+    if (!contactOrder) {
+      toast({
+        description: '請選擇訂單',
+        variant: 'destructive',
+      })
+      return
+    }
+
+    try {
+      await createMemberFeedback({
+        id: contactOrder.id!,
+        domain_id: contactOrder.domain_id!,
+        ordergroup_id: Number(contactOrder.ordergroupnumber),
+        name: user?.name || '',
+        email: user?.email || '',
+        tel: user?.tel || '',
+        mobile: user?.mobile || '',
+        title: '訂單留言',
+        memo: contactMessage,
+        retitle: '',
+        rebody: '',
+        redate: '',
+        member_id: user?.id || 0,
+        adminuser_id: 0,
+        adminuser_name: '',
+        created_at: format(new Date(), 'yyyy-MM-dd'),
+        updated_at: '',
+      })
+      toast({
+        description: '發送成功',
+        className: 'bg-cyan-500 text-white',
+      })
+    } catch (error) {
+      console.error(error)
+      toast({
+        description: `發送失敗: ${error}`,
+        variant: 'destructive',
+      })
+    }
+  }
+
   return (
-    <OrderContext.Provider value={{ orders, isLoadingOrders }}>{children}</OrderContext.Provider>
+    <OrderContext.Provider
+      value={{
+        orders,
+        contactOrder,
+        contactMessage,
+        contactTextareaRef,
+        isLoadingOrders,
+        isContactDialogOpen,
+        handleContactDialogClose,
+        handleContactDialogOpen,
+        handleContactMessageChange,
+        handleContactSubmit,
+      }}
+    >
+      {children}
+    </OrderContext.Provider>
   )
 }
 
--- services/feedback.ts
+++ services/feedback.ts
@@ -30,29 +30,34 @@ type getFeedbackRes = ApiRes<{
   data: FeedbackData[]
 }>
 
-export const createMemberFeedback = async () => {
+export const createMemberFeedback = async (feedback: FeedbackData) => {
   const { data: res } = await axiosInstance.post<createFeedbackRes>(
     'api/membercenter/feedback/store',
-    {
-      id: 1,
-      domain_id: 1,
-      ordergroup_id: 10001,
-      name: 'test',
-      email: 'test@gmail.com',
-      tel: '0212345678',
-      mobile: '0912345678',
-      title: 'test',
-      memo: 'test',
-      retitle: 'test',
-      rebody: 'test',
-      redate: '2021-08-01',
-      member_id: 1,
-      adminuser_id: 1,
-      adminuser_name: 'admin',
-      created_at: '2021-08-12',
-      updated_at: '2021-08-01',
-    },
+    feedback,
+    // {
+    //   id: 1,
+    //   domain_id: 1,
+    //   ordergroup_id: 10001,
+    //   name: 'test',
+    //   email: 'test@gmail.com',
+    //   tel: '0212345678',
+    //   mobile: '0912345678',
+    //   title: 'test',
+    //   memo: 'test',
+    //   retitle: 'test',
+    //   rebody: 'test',
+    //   redate: '2021-08-01',
+    //   member_id: 1,
+    //   adminuser_id: 1,
+    //   adminuser_name: 'admin',
+    //   created_at: '2021-08-12',
+    //   updated_at: '2021-08-01',
+    // },
   )
+
+  if (res.resultcode !== 0) {
+    throw new Error(res.resultmessage)
+  }
 
   return res.data
 }
