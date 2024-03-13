a6f1077193f9cde01b5571cd1e1a9fb653ce6f13
OInVein <progthrash3305@gmail.com>
2024-03-13 23:20:44 +0800

    fix(layout): recover touch-none layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,9 +44,13 @@ export default function RootLayout({
           <CartProvider>
             <AddressProvider>
               <body
-                className={cn('bg-black font-sans antialiased', inter.variable, inter.className)}
+                className={cn(
+                  'touch-none bg-black font-sans antialiased',
+                  inter.variable,
+                  inter.className,
+                )}
               >
-                <div className="mx-auto min-h-screen w-full max-w-md">{children}</div>
+                <div className="max-w-mdtouch-none mx-auto min-h-screen w-full">{children}</div>
                 <Toaster />
               </body>
             </AddressProvider>
