a9ffc4c50f6835b9438af99eab7762d492397a44
OInVein <progthrash3305@gmail.com>
2024-03-18 00:22:14 +0800

    feat(NavigationContext): add NavigationContext for recording last page

--- app/info/LoginTab.tsx
+++ app/info/LoginTab.tsx
@@ -4,15 +4,18 @@ import { useRouter } from 'next/navigation'
 import { ChevronRightIcon } from 'lucide-react'
 
 import { useAuthContext } from '@/context/AuthContext'
+import { useNavigationContext } from '@/context/NavigationContext'
 
 const LoginTab = () => {
   const router = useRouter()
   const { isLogin, handleLogout } = useAuthContext()
+  const { setFromPath } = useNavigationContext()
 
   function toggleLogin() {
     if (isLogin) {
       handleLogout()
     } else {
+      setFromPath('/')
       router.push('/login')
     }
   }
