df24d9b6feadbc851e982cc9994e3aa99b8099a5
OInVein <progthrash3305@gmail.com>
2024-03-19 00:26:22 +0800

    feat(profile): add tiktok status button and add address button

--- app/profile/ProfileFormItem.tsx
+++ app/profile/ProfileFormItem.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { HTMLInputTypeAttribute } from 'react'
+import { HTMLInputTypeAttribute, PropsWithChildren, ReactNode } from 'react'
 import { z } from 'zod'
 import { ControllerRenderProps } from 'react-hook-form'
 import { ExternalLink } from 'lucide-react'
@@ -10,6 +10,33 @@ import { FormControl, FormMessage } from '@/components/ui/form'
 import { useAuthContext } from '@/context/AuthContext'
 import { Skeleton } from '@/components/ui/skeleton'
 import { cn } from '@/lib/utils'
+
+export function ProfileFormItemLayout({
+  children,
+  label,
+}: PropsWithChildren<{
+  label: ReactNode
+}>) {
+  const { isLoadingUser } = useAuthContext()
+  return (
+    <div className="flex min-h-12 items-center justify-between bg-white p-4 py-0 md:min-h-14">
+      <span className="flex items-center gap-2 text-gray-500">{label}</span>
+
+      <div className="flex flex-col items-end gap-1">
+        <FormControl className="max-w-48 flex-[1] md:max-w-60">
+          {isLoadingUser ? (
+            <div className="flex justify-end">
+              <Skeleton className="h-6 w-36" />
+            </div>
+          ) : (
+            children
+          )}
+        </FormControl>
+        <FormMessage />
+      </div>
+    </div>
+  )
+}
 
 export const formSchema = z.object({
   id: z.string(),
@@ -41,40 +68,26 @@ export default function ProfileFormItem<T extends FormKeys>({
   type = 'text',
   onClick,
 }: ProfileFormItemProps<T>) {
-  const { isLoadingUser } = useAuthContext()
   return (
-    <div className="flex min-h-12 items-center justify-between bg-white p-4 py-0 md:min-h-14">
-      <span className="flex items-center gap-2">{label}</span>
-
-      <div className="flex flex-col items-end gap-1 py-2">
-        <FormControl className="max-w-48 flex-[1] md:max-w-60">
-          {isLoadingUser ? (
-            <div className="flex justify-end">
-              <Skeleton className="h-6 w-36" />
-            </div>
-          ) : (
-            <div
-              onClick={onClick}
-              className={cn('flex items-center gap-2', {
-                'cursor-pointer': readOnly,
-              })}
-            >
-              <Input
-                className={cn('appearance-none bg-white text-right md:text-base', {
-                  'border-none p-0 outline-none': disabled || readOnly,
-                  'cursor-pointer text-gray-400': readOnly,
-                })}
-                disabled={disabled}
-                readOnly={readOnly}
-                type={type}
-                {...field}
-              />
-              {type === 'password' && <ExternalLink className="cursor-pointer text-gray-400" />}
-            </div>
-          )}
-        </FormControl>
-        <FormMessage />
+    <ProfileFormItemLayout label={label}>
+      <div
+        onClick={onClick}
+        className={cn('flex items-center gap-2', {
+          'cursor-pointer': readOnly,
+        })}
+      >
+        <Input
+          className={cn('appearance-none bg-white text-right md:text-base', {
+            'border-none p-0 outline-none': disabled || readOnly,
+            'cursor-pointer text-gray-400': readOnly,
+          })}
+          disabled={disabled}
+          readOnly={readOnly}
+          type={type}
+          {...field}
+        />
+        {type === 'password' && <ExternalLink className="cursor-pointer text-gray-400" />}
       </div>
-    </div>
+    </ProfileFormItemLayout>
   )
 }
