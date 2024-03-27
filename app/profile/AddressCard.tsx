49954e86b1be14a52ad72789fdc1679105307b51
OInVein <progthrash3305@gmail.com>
2024-03-27 08:40:21 +0800

    fix(AddressCard): correct clickable area

--- app/profile/AddressCard.tsx
+++ app/profile/AddressCard.tsx
@@ -12,8 +12,8 @@ type AddressCardProps = {
 
 function AddressCard({ address, onClick, onDelete }: AddressCardProps) {
   return (
-    <section className="flex cursor-pointer items-center justify-between rounded-xl bg-white p-4">
-      <div className="flex flex-col">
+    <section className="flex items-center justify-between rounded-xl bg-white p-4">
+      <div className="flex cursor-pointer flex-col" onClick={onClick}>
         <span className="flex items-center gap-2">
           <span className="grid h-8 w-auto place-items-center rounded-xl bg-primary-foreground px-2 text-primary">
             {address.CVSStoreName || '取貨地點'}
@@ -24,7 +24,7 @@ function AddressCard({ address, onClick, onDelete }: AddressCardProps) {
         <span className="text-gray-500">{address.CVSAddress || address.address}</span>
       </div>
       <div className="flex space-x-2">
-        <SquarePen className="mt-2 text-gray-500" onClick={onClick} />
+        <SquarePen className="mt-2 cursor-pointer text-gray-500" onClick={onClick} />
         <ConfirmDeleteDialog onConfirm={onDelete} />
       </div>
     </section>
